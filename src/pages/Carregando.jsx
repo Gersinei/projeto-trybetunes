7a4e80a93ee032633010d3f746e38fc98a018d9b
gersinei <gersinei@yahoo.com>
2022-12-20 20:53:05 -0300

    requisito

---
+++ src/pages/Carregando.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Carregando extends Component {
+  render() {
+    return (
+      <p>
+        Carregando...
+      </p>
+    );
+  }
+}
+
+export default Carregando;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+
+import { createUser } from '../services/userAPI';
+
+import Carregando from './Carregando';
+
 class Login extends Component {
+  state = {
+    name: '',
+    carregando: false,
+  };
+
   render() {
+    const { name, carregando } = this.state;
+    const { history } = this.props;
+    const gg = 3;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form action="">
+          {
+            carregando && <Carregando />
+          }
+          <input
+            data-testid="login-name-input"
+            type="text"
+            onChange={ ({ target }) => this.setState({ name: target
+              .value }) }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ name.length < gg }
+            onClick={ async () => {
+              this.setState({
+                carregando: true,
+              });
+              await createUser({ name });
+              return history.push('/search');
+            } }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Login;

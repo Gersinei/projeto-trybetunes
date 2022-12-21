891a95bc65cf979a009b01e8f627509dcbad9d3a
gersinei <gersinei@yahoo.com>
2022-12-20 22:51:06 -0300

    requisito05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    nameArtist: '',
+  };
+
   render() {
+    const { nameArtist } = this.state;
+    const zeze = 2;
     return (
       <>
         <Header />
         <div data-testid="page-search" />
+        <form>
+          <input
+            type="text"
+            onChange={ ({ target }) => this.setState({ nameArtist: target.value }) }
+            data-testid="search-artist-input"
+          />
+
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ nameArtist.length < zeze }
+          >
+            Pesquisar
+          </button>
+        </form>
       </>
     );
   }

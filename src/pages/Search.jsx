a28f3c9ef82289098231010734b0797327e7d9d2
gersinei <gersinei@yahoo.com>
2022-12-21 21:50:48 -0300

    requisito06

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,34 +1,86 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends Component {
   state = {
+    retornoDaApi: true,
     nameArtist: '',
+    carregando: false,
+    artistas: [],
+  };
+
+  pesquisar = async () => {
+    this.setState({
+      carregando: true,
+    });
+    const { nameArtist } = this.state;
+    const data = await searchAlbumsAPI(nameArtist);
+    if (data.length <= 0) {
+      this.setState({
+        retornoDaApi: false,
+        carregando: false,
+      });
+    } else {
+      this.setState({
+        carregando: false,
+        artistas: data,
+        retornoDaApi: true,
+      });
+    }
   };
 
   render() {
-    const { nameArtist } = this.state;
+    const { nameArtist, carregando, artistas, retornoDaApi } = this.state;
     const zeze = 2;
     return (
-      <>
-        <Header />
-        <div data-testid="page-search" />
-        <form>
-          <input
-            type="text"
-            onChange={ ({ target }) => this.setState({ nameArtist: target.value }) }
-            data-testid="search-artist-input"
-          />
 
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ nameArtist.length < zeze }
-          >
-            Pesquisar
-          </button>
-        </form>
-      </>
+      <div data-testid="page-search">
+        <Header />
+        { carregando ? <Carregando />
+          : (
+            <div>
+              <form>
+                <input
+                  type="text"
+                  onChange={ ({ target }) => this.setState({ nameArtist: target.value }) }
+                  data-testid="search-artist-input"
+                />
+
+                <button
+                  onClick={ this.pesquisar }
+                  type="button"
+                  data-testid="search-artist-button"
+                  disabled={ nameArtist.length < zeze }
+                >
+                  Pesquisar
+                </button>
+                <p>
+                  Resultado de álbuns de:
+                  {' '}
+                  { nameArtist }
+                  {' '}
+                </p>
+              </form>
+              { !retornoDaApi && <p>Nenhum álbum foi encontrado</p> }
+              {artistas.map((element, index) => (
+                <div key={ index }>
+                  <img src={ element.artworkUrl100 } alt="foto-album" />
+                  <Link
+                    data-testid={ `link-to-album-${element.collectionId}` }
+                    to={ `/album/${element.collectionId}` }
+                  >
+                    {element.collectionName}
+                  </Link>
+                </div>
+              ))}
+
+            </div>
+          )}
+      </div>
+
     );
   }
 }

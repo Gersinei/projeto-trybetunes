21fe85945855fee9a5cb2c385c853abc6cdb0b2e
gersinei <gersinei@yahoo.com>
2023-01-02 17:20:24 -0300

    requisito08

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,17 +1,30 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Carregando from '../pages/Carregando';
 import { addSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
+  state = {
+    carregando: false,
+  };
+
   adicionarMusicas = async () => {
     const { musicas } = this.props;
+    this.setState({
+      carregando: true,
+    });
     await addSong(musicas);
+    this.setState({
+      carregando: false,
+    });
   };
 
   render() {
     const { musicas } = this.props;
+    const { carregando } = this.state;
     return (
       <div>
+        { carregando && <Carregando /> }
         <p>
           {' '}
           {musicas.trackName}
@@ -30,6 +43,7 @@ export default class MusicCard extends Component {
             type="checkbox"
             id={ musicas.trackId }
             onChange={ this.adicionarMusicas }
+
           />
           Favorita
         </label>
